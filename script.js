5a9f60c98bf0762eb4f1541c0aa0c1ac4ffc7b8d
Jéssica Florentino <jdgflorentino@gmail.com>
2021-12-10 16:19:45 -0300

    5 - Adicione um botão com id=criar-tarefa e, ao clicar nesse botão, um novo item deverá ser criado ao final da lista e o texto do input deve ser limpo.

---
+++ script.js
@@ -0,0 +1,19 @@
+function Add() {
+    let textInput = document.querySelector('#texto-tarefa');
+    let addButton = document.querySelector('#criar-tarefa');
+    let listTask = document.querySelector('#lista-tarefas');
+
+    addButton.addEventListener('click', function() {
+        if (textInput.value.length > 0) {
+            let newLi = document.createElement('li');
+            newLi.innerText = textInput.value;
+
+            listTask.appendChild(newLi);
+            textInput.value = '';
+        } else {
+            alert('Error: Digite ao menos 1 caractere.');
+        }
+    });
+}
+
+Add();
